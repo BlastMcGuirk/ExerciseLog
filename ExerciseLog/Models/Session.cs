bef5bdca9149ad979644b535a280743630435081
Brendan McGuirk <Brendan.McGuirk@jacobs.com>
2023-12-08 10:26:42 -0500

    Added adding an exercise to a workout

---
+++ ExerciseLog/Models/Session.cs
@@ -0,0 +1,13 @@
+﻿namespace ExerciseLog.Models
+{
+    public class Session
+    {
+        public int Id { get; set; }
+
+        public DateTime SessionDate { get; set; }
+
+        public Workout Workout { get; set; }
+
+        public List<Set> Sets { get; set; }
+    }
+}
